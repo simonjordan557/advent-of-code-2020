6b6fef19788fd4f329d874c50c146063f6f4b168
Simon Jordan <simonjordan557@aol.com>
2020-12-15 17:23:29 +0000

    Neary complete Day 4 Challenge 1. Need to squash an empty array entry bug

--- AdventOfCodeHelper/AdventOfCodeHelper/Helper.cs
+++ AdventOfCodeHelper/AdventOfCodeHelper/Helper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.IO;
 using System.Collections.Generic;
 
@@ -15,6 +16,31 @@ namespace AdventOfCodeHelper
             while (!sr.EndOfStream)
             {
                 rawData.Add(sr.ReadLine());
+            }
+            sr.Close();
+            return rawData;
+        }
+
+        public static List<string> ReadInDataAndSeparateWhenBlankLine(string fileToOpen)
+        {
+            // Read in raw data from .txt file, separate when a blank line is encountered, hold in collection
+            StringBuilder sb = new StringBuilder();
+            List<string> rawData = new List<string>();
+            FileStream fs = File.Open(fileToOpen, FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    sb.Append(line);
+                    sb.Append(" ");
+                }
+                else
+                {
+                    rawData.Add(sb.ToString());
+                    sb.Clear();
+                }
             }
             sr.Close();
             return rawData;
